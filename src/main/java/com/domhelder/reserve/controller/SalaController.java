409dca2f67100e1ef9195ce51405c28c51526947
JaoNotLitz <128253740+JaoNotLitz@users.noreply.github.com>
2025-06-11 22:57:29 -0300

    fix: cors cross policy fixed

--- src/main/java/com/domhelder/reserve/controller/SalaController.java
+++ src/main/java/com/domhelder/reserve/controller/SalaController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/sala")
 public class SalaController {
