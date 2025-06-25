340008758c851e56c3948b9ddc63f653f2a09703
JaoNotLitz <128253740+JaoNotLitz@users.noreply.github.com>
2025-06-25 15:01:42 -0300

    feat: "Horarios" updated

--- src/main/java/com/domhelder/reserve/entity/Horarios.java
+++ src/main/java/com/domhelder/reserve/entity/Horarios.java
@@ -1,6 +1,15 @@
 package com.domhelder.reserve.entity;
 
 public enum Horarios {
+        H07_40,
+        H09_40,
+        H13_00,
+        H15_00,
+        H18_20,
+        H20_20,
+        //  THESE TIMES BELLOW IS FOR TESTS ONLY, AND WILL BE MAINTAINED
+        //  ||
+        //  \/
         H08_00,
         H08_50,
         H10_00,
